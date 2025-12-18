94f33aff886db73125ca1695bd7b4038d9a74781
itz-harsh <hsuthar1125@gmail.com>
2025-12-19 00:56:33 +0530

    test3

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
-REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+REDIRECT_URI = "https://spotify-to-musify.vercel.app/"
 
 def export_playlist(playlist_id):
     """
